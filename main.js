875e9d03749c1dba3431e7aab38ccf39159b0007
JFonS <joan.fonssanchez@gmail.com>
2015-09-03 00:24:49 +0200

    Screen resize

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 // create an new instance of a pixi stage
-var stage = new PIXI.Container(0xFF0000);
+var stage = new PIXI.Container();
 
 // create a renderer instance
-var renderer = PIXI.autoDetectRenderer(800, 800);
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, Math.max(550,window.innerHeight)-20);
+
+window.onresize = function (event){
+    var w = window.innerWidth;
+    var h = Math.max(550,window.innerHeight)-20;
+
+    //this part resizes the canvas but keeps ratio the same
+    renderer.view.style.width = w + "px";
+    renderer.view.style.height = h + "px";
+
+    //this part adjusts the ratio:
+    renderer.resize(w,h);
+    setTimeout(redraw,300);
+}
 
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
+renderer.backgroundColor = 0xdfdfdf;
 //requestAnimationFrame(animate);
 
 var kernel = [{
@@ -22,12 +36,17 @@ var kernel = [{
     y: 30
 }];
 
-var sides = 3;
+var sides = 4;
 
-var container = drawTiled(kernel, sides, {x: 0, y: 0}, {x: window.innerWidth, y: 500});
-stage.addChild(container);
 
-renderer.render(stage);
+function redraw(){
+    var container = drawTiled(kernel, sides, {x: 0, y: 250}, {x: window.innerWidth, y: 500});
+    stage.addChild(container);
+
+    renderer.render(stage);
+}
+
+redraw();
 
 /*function animate() {
     requestAnimationFrame(animate);
